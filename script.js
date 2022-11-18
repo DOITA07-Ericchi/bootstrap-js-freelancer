2dd746fe2e26fef1b3b57590a00152cf2152d998
EricchiYukia <git@ericchi.moe>
2022-11-18 15:24:23 +0100

    Finito il superbonus, iniziata la base

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 
 // Parte che riciclerò per fare la pagina dinamica
-const websites = [ {
+const sito = [ {
     nome: "Cabin website",
-    foto: "cabin.png"
+    foto: "cabin.png",
+    urlo: "https://ericchi.moe"
 }, {
     nome: "Cake website",
-    foto: "cake.png"
+    foto: "cake.png",
+    urlo: "https://ericchi.moe"
 }, {
     nome: "Circus website",
-    foto: "circus.png"
+    foto: "circus.png",
+    urlo: "https://ericchi.moe"
 }, {
     nome: "Game website",
-    foto: "game.png"
+    foto: "game.png",
+    urlo: "https://ericchi.moe"
 }, {
     nome: "Safe website",
-    foto: "safe.png"
+    foto: "safe.png",
+    urlo: "https://ericchi.moe"
 }, {
     nome: "Submarine website",
-    foto: "submarine.png"
+    foto: "submarine.png",
+    urlo: "https://ericchi.moe"
 } ]
 
 function stampaMembro(sito){
@@ -27,10 +33,28 @@ Foto:  ${sito.foto}`)
 
 function renderizzaMembro(sito){
     let codice = document.getElementById("tabellaSiti");
-    codice.innerHTML += // Qua ci va il codice delle card
+    codice.innerHTML += `<div class="col-12 col-md-6 col-lg-4">
+    <div class="card">
+    <img src="img/portfolio/` + sito.foto + `" class="card-img-top" alt="`+ sito.nome +`">
+    <div class="card-body">
+    <h5 class="card-title">` + sito.nome +`</h5>
+    <a href="#" class="btn btn-primary btn-info">Preview</a>
+    <a href="` + sito.urlo +`" class="btn btn-outline-info">Visit site</a>
+    </div>
+    </div>
+    </div>`
 }
 
-for (let sito of websites) {
-    stampaMembro(sito);
-    renderizzaMembro(sito);
+function calcolaForm(){
+
+}
+
+function submitForm(event){
+    event.preventDefault();
+    
+}
+
+for (let elemento of sito) {
+    stampaMembro(elemento);
+    renderizzaMembro(elemento);
 }
